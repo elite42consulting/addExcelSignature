1df3fb3fdee25341108e1255827c2a14a124a563
Andrew Sauder <andrewsauder@gmail.com>
2024-01-05 13:21:44 -0500

    add installer and icon

--- addExcelSignature/Properties/AssemblyInfo.cs
+++ addExcelSignature/Properties/AssemblyInfo.cs
@@ -7,10 +7,10 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("addExcelSignature")]
+[assembly: AssemblyTitle("Sign Excel Document")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Elite 42 Consulting LLC")]
 [assembly: AssemblyProduct("addExcelSignature")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
